7012db5bf8da226907a5e3cb52ad4dac2246c60b
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-03-19 18:58:54 +0100

    Consumer Configuration

---
+++ src/main/java/com/example/springkafkaconsumerv1/consumer/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package com.example.springkafkaconsumerv1.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "NewTopic", groupId = "group_id")
+    public void consume(String  message){
+        System.out.println("message: " + message);
+    }
+}
---
+++ src/test/java/com/example/springkafkaconsumerv1/SpringKafkaConsumerV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkaconsumerv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaConsumerV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
